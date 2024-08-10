39e4ec9fcbb3e6d036f0803e8dedb19976b73cf1
Pallab <pallabkshyp@gmail.com>
2024-08-10 18:43:12 +0530

    rate limiting on api requests

--- server/app.js
+++ server/app.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const rateLimite = require('express-rate-limit');
 const authRouter = require('./routes/authRoutes');
 const webhookRouter = require('./routes/webhookRoutes')
-
 const publicationRouter = require('./routes/publicationRoutes')
 
 const app = express();
 
+const limiter = rateLimite({
+    max: 50,
+    windowMs: 60*1000,
+    message: 'to many request'
+})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', authRouter)
-app.use('/webhook', webhookRouter)
+app.get('/testLimit', limiter, (req, res) => res.send({message: 'success'}));
+app.use('/api/auth', authRouter)
+app.use('/api/webhook', webhookRouter)
 app.use('/api/publications', publicationRouter)
 
 app.listen(3000, ()=> console.log('server started'))
