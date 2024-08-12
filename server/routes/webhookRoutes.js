b90995a9272cb3d2edbab754be601dcf37358c9b
Pallab <pallabkshyp@gmail.com>
2024-08-12 22:52:27 +0530

    webhook update

--- server/routes/webhookRoutes.js
+++ server/routes/webhookRoutes.js
@@ -1,6 +1,7 @@
 const express =  require('express')
 const router = express.Router();
 const {removeUserAuthToken,deleteUser,updateUserPublication,updatePublication} = require('../controller/webhookController')
+const  checkServerToken  = require('../middleware/checkServerToken')
 
 router.route('/update_user')
     .post(checkServerToken, removeUserAuthToken)
