1ca0cb9781bfa2172d4450bfa0089f35b2a29e30
Pallab Kashyap <141304625+Pallab-Kashyap@users.noreply.github.com>
2024-10-13 17:44:35 +0530

    Delete server directory
    removing backend

--- server/config/database.js
+++
@@ -1,9 +0,0 @@
-// // config/database.js
-// const { Sequelize } = require('sequelize');
-
-// const sequelize = new Sequelize('osborne_books', 'your_username', 'your_password', {
-//     host: 'localhost',
-//     dialect: 'postgres',
-// });
-
-// module.exports = sequelize;
--- server/utils/asyncWarpper.js
+++
@@ -1,5 +0,0 @@
-const asyncWrapper = (fn) => {
-    return (req, res, next) => {
-      fn(req, res, next).catch(error => next(error));
-    };
-  };
